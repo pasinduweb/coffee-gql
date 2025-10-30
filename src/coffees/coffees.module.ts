8defd2a583ec6ff222d2ee3608f0be63d92c4e92
Pasindu Manaranjana <algopasindu@gmail.com>
2025-10-31 00:19:39 +0530

    init CoffeesResolver and integrate with CoffeesModule

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { CoffeesResolver } from './coffees.resolver';
 
-@Module({})
+@Module({
+  providers: [CoffeesResolver],
+})
 export class CoffeesModule {}
