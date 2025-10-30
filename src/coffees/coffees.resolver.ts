523031c6d0286a9af2edde527dbdc158f84e9a24
Pasindu Manaranjana <algopasindu@gmail.com>
2025-10-31 00:25:15 +0530

    add findAll query to CoffeesResolver for retrieving all coffees

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,4 +1,10 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Coffee } from './entities/coffee.entity';
 
 @Resolver()
-export class CoffeesResolver {}
+export class CoffeesResolver {
+  @Query(() => [Coffee], { name: 'coffees' })
+  async findAll() {
+    return [];
+  }
+}
