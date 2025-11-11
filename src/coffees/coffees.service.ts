824f5e8bd4d3fd0a5e982f5ad79c9324001fce2c
Pasindu Manaranjana <algopasindu@gmail.com>
2025-11-11 21:18:01 +0530

    implement actual DB operations in CoffeesService methods, remove nullable option

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCoffeeInput } from './dto/create-coffee.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Repository } from 'typeorm';
+import { UserInputError } from '@nestjs/apollo';
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeesRepository: Repository<Coffee>,
+  ) {}
+
   async findAll() {
-    return [];
+    return this.coffeesRepository.find();
   }
 
   async findOne(id: number) {
-    return null;
+    const coffee = await this.coffeesRepository.findOne({ where: { id } });
+    if (!coffee) {
+      throw new UserInputError(`Coffee ${id} does not exist`);
+    }
+    return coffee;
   }
 
   async create(createCoffeeInput: CreateCoffeeInput) {
-    return null;
+    const coffee = this.coffeesRepository.create(createCoffeeInput);
+    return this.coffeesRepository.save(coffee);
   }
 }
