ceebd2e6069953291faa7d5bf4d51c5ab689ad0c
Pasindu Manaranjana <algopasindu@gmail.com>
2025-11-12 18:11:20 +0530

    add validation for name field and fix typo

--- src/coffees/dto/create-coffee.input.ts
+++ src/coffees/dto/create-coffee.input.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 
-@InputType({ description: 'Create coffee input object tyle.' })
+@InputType({ description: 'Create coffee input object style.' })
 export class CreateCoffeeInput {
+  @MinLength(3)
   @Field(() => String, { description: 'A new coffee name' })
   name: string;
   brand: string;
